6e81099cd189131796ba8474a5c2705f5f61c4b4
Nicolas <76406182+n1colass@users.noreply.github.com>
2021-12-08 15:40:27 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* $(document).ready(function () {
+    $(".dec-3").hover(() =>{
+        $(".dec-3").fadeIn(1000,() =>{
+            $(".dec-3").addClass("animation-3");
+        });
+    }, function() { 
+        
+            $(".dec-3").removeClass("animation-3");
+        
+    });
+}); */
